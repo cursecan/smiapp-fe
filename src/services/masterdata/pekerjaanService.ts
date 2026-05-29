e46e8ee1c514e01cabfb328b61df74550275524f
cursecan <anderi.setiawan@gmail.com>
2026-05-29 09:03:32 +0700

    Update limit list

--- src/services/masterdata/pekerjaanService.ts
+++ src/services/masterdata/pekerjaanService.ts
@@ -4,7 +4,7 @@ import { api } from '../../lib/api'
 export const usePekerjaanService = {
     list: ({queryKey}) => {
         const [, q, pelabuhan] = queryKey
-        return api.get('/master/pekerjaan/', {params: {pelabuhan, q}})
+        return api.get('/master/pekerjaan/', {params: {limit: 100, pelabuhan, q}})
     },
     detail: (id) => {
         return api.get(`/master/pekerjaan/${id}/`)
