3ce7ffa500f207451760e672bd16d601b1439818
cursecan <anderi.setiawan@gmail.com>
2026-07-13 18:09:08 +0700

    Add fix

--- src/routes/_protected/oprasional/-components/DownloadBAST.tsx
+++ src/routes/_protected/oprasional/-components/DownloadBAST.tsx
@@ -2,6 +2,8 @@ import { Button } from "@heroui/react"
 import { api } from "../../../../lib/api"
 
 const DownloadBAST = ({data}) => {
+    console.log(data);
+    
     const handleDownload = async () => {
         const res = await api.get(`oprasional/bast/${data}/download/`, {responseType: 'blob'})
         const url = window.URL.createObjectURL(
@@ -11,7 +13,7 @@ const DownloadBAST = ({data}) => {
         const link = document.createElement('a')
 
         link.href = url
-        const filename = `bast.pdf`
+        const filename = `${data}.pdf`
         link.setAttribute('download', filename)
 
         document.body.appendChild(link)
