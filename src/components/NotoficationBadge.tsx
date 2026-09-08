f465ade5a91f5fd6dccdaff224fa5bec3b600535
unknown <anderi.setiawan@gmail.com>
2026-09-08 10:16:26 +0700

    Fix notif

--- src/components/NotoficationBadge.tsx
+++ src/components/NotoficationBadge.tsx
@@ -1,12 +1,44 @@
 import { Bell, Dots9, FloppyDisk } from "@gravity-ui/icons"
-import { Badge, Button, Popover, Surface } from "@heroui/react"
+import { Avatar, Badge, Button, Disclosure, Popover, Surface } from "@heroui/react"
 import { useQuery } from "@tanstack/react-query"
 import { useNotificationService } from "../services/notification/notificationService"
 import { useMemo } from "react"
 import { Link as LinkHero } from "@heroui/react"
 import { Link } from "@tanstack/react-router"
+import { useAuth } from "../auth/AuthProvider"
+
+
+
+const DiscloseNotif = ({header='Preview HeroUI Native', data=[]}) => {
+    return (
+        <Disclosure>
+            <Disclosure.Heading>
+                <Button slot="trigger" variant="secondary">
+                    {/* <QrCode /> */}
+                    { header } ({data.length})
+                    <Disclosure.Indicator />
+                </Button>
+            </Disclosure.Heading>
+            <Disclosure.Content>
+                <Disclosure.Body className="shadow-panel flex flex-col gap-2 max-h-64 overflow-auto">
+                    {
+                        data.map((i, index) => (
+                            <Link className="link text-gray-600" to={i.path} key={i.id}>
+                                <p>{i.active_approval.name} - {i.description}</p>
+                                <LinkHero.Icon />
+                            </Link>
+                        ))
+                    }
+                </Disclosure.Body>
+            </Disclosure.Content>
+        </Disclosure>
+    )
+}
+
+
 
 const NotoficationBadge = () => {
+    const { user } = useAuth()
     const {data} = useQuery({
         queryKey: ['notification'],
         queryFn: ()  => useNotificationService.list(),
@@ -15,7 +47,6 @@ const NotoficationBadge = () => {
         refetchIntervalInBackground: true,
     })
 
-    console.log(data);
 
     const notifData = useMemo(() => {
         const grouped = data?.reduce((result, item) => {
@@ -33,7 +64,6 @@ const NotoficationBadge = () => {
         return grouped
     })
 
-    console.log(notifData);
     
     
 
@@ -51,10 +81,37 @@ const NotoficationBadge = () => {
                 )
             }
         </Badge.Anchor>
-        <Popover.Content placement="right" className="max-w-64">
+        <Popover.Content placement="right" className="max-w-90 w-full">
             <Popover.Dialog>
                 <Popover.Arrow />
-                <div className="flex flex-col gap-4">
+                <Popover.Heading>
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center gap-3">
+                        <Avatar size="md">
+                            <Avatar.Image
+                            alt=""
+                            src="https://img.heroui.chat/image/avatar?w=400&h=400&u=1"
+                            />
+                            <Avatar.Fallback>SJ</Avatar.Fallback>
+                        </Avatar>
+                        <div>
+                            <p className="font-semibold">{ user.full_name }</p>
+                            <p className="text-sm text-muted">@{user.username}</p>
+                        </div>
+                        </div>
+                    </div>
+                </Popover.Heading>
+                <div className="mt-3 space-y-4">
+                    { 
+                        notifData && Object.entries(notifData).map(([group, items]) => {
+                            return (
+                                <DiscloseNotif key={group} header={group} data={items} />
+                            )
+                        })
+                    }
+                </div>
+
+                {/* <div className="flex flex-col gap-4">
                     { 
                         notifData && Object.entries(notifData).map(([group, items]) => {
                             return (
@@ -76,7 +133,7 @@ const NotoficationBadge = () => {
                             )
                         })
                     }
-                </div>
+                </div> */}
             </Popover.Dialog>
         </Popover.Content>
     </Popover>
